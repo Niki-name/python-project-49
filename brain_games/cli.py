7d542dbdd35fa3d496eb1676b0a1fd31fd3db1dc
Nikita Kulikov <brazefly@gmail.com>
2023-01-24 23:14:22 +1000

    reinstall WHL

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,6 +2,6 @@ import prompt
 
 
 def welcome_user():
-    print('brain-games \nWelcome to the Brain Games !')
+    print('brain-games \nWelcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name} !')
